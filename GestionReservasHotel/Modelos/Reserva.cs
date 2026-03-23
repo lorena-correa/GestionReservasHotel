848f4a23e089406e498b9b7ce7c9aa573ba02046
leidy-correa <lorena99lozano@email.com>
2026-03-22 22:35:02 -0500

    Se agregaron propiedades a la clase reserva

--- GestionReservasHotel/Modelos/Reserva.cs
+++ GestionReservasHotel/Modelos/Reserva.cs
@@ -8,5 +8,18 @@ namespace GestionReservasHotel.Modelos
 {
     internal class Reserva
     {
+        public string NombreCliente { get; set; }
+        public string DocumentoCliente { get; set; }
+        public int NumeroHabitacion { get; set; }
+        public DateTime FechaReserva { get; set; }
+        public int DuracionEstadia { get; set; }
+        public decimal TarifaPorNoche { get; set; }
+
+        public virtual decimal CalcularCostoTotal()
+        {
+            return TarifaPorNoche * DuracionEstadia;
+        }
+
+
     }
 }
