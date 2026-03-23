280ce5471274509976b76a4ae91525faf68e0998
leidy-correa <lorena99lozano@email.com>
2026-03-22 23:34:39 -0500

    Se creas el proyecto inventario  y se valida la opcion cuando una habitación ya esta previamente reservada

--- GestionReservasHotel/Servicios/ReservaService.cs
+++ GestionReservasHotel/Servicios/ReservaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestionReservasHotel.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace GestionReservasHotel.Servicios
 {
-    internal class ReservaService
-    {
+    internal class ReservaService{
+        private List<Reserva> reservas = new List<Reserva>();
+
+        public List<Reserva> ObtenerTodas()
+        {
+            return reservas;
+        }
+
+        public void Agregar(Reserva nueva)
+        {
+            nueva.ValidarDatos();
+
+            if (ExisteConflicto(nueva))
+                throw new Exception("Conflicto: habitación ya reservada en esas fechas");
+
+            reservas.Add(nueva);
+        }
+
     }
 }
